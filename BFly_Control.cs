4ad3f2fc1bd25badb0b82697a141f1afa3331be9
Mark Raper <mrkraper@gmail.com>
2016-07-28 20:08:46 +0100

    Stick in some code

---
+++ BFly_Control.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BFly_Control : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+    public float speed;
+
+
+    void Start () 
+	{
+
+
+    rb2d = GetComponent<Rigidbody2D> ();
+
+    }
+	
+
+	void FixedUpdate ()
+	{
+		
+		float MoveButtY = Input.GetAxisRaw ("Horizontal");
+		float MoveButtX = Input.GetAxisRaw ("Vertical");
+
+		Vector2 movement = new Vector2 (MoveButtY, MoveButtX);
+
+        rb2d.AddForce(movement * speed);
+
+        //8 directional movement
+        if (MoveButtY > 0 && MoveButtX > 0)
+        {
+            rb2d.MoveRotation(-45);
+        }
+
+        else if (MoveButtY < 0 && MoveButtX > 0)
+        {
+            rb2d.MoveRotation(45);
+        }
+
+        else if (MoveButtY > 0 && MoveButtX < 0)
+        {
+            rb2d.MoveRotation(-135);
+        }
+
+        else if (MoveButtY < 0 && MoveButtX < 0)
+        {
+            rb2d.MoveRotation(135);
+        }
+        else if (MoveButtY > 0)
+        {
+            rb2d.MoveRotation(-90);
+        }
+        else if (MoveButtY < 0){ 
+            
+            rb2d.MoveRotation(90);
+               }
+        else if (MoveButtX > 0)
+        {
+            rb2d.MoveRotation(0);
+        }
+        else if (MoveButtX < 0)
+        {
+            rb2d.MoveRotation(180);
+        }
+        
+          
+        
+	}
+
+
+}
---
+++ BirdMove.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMove : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    public float moveSpeed;
+    public float stopSpeed;
+    private bool touchDown1;
+    private bool touchDown2;
+    private bool touchDown3;
+
+    void CheckTouch1() {
+        touchDown1 = true;
+        Debug.Log(touchDown1);
+    }
+
+    void CheckTouch2() {
+        touchDown2 = true;
+        Debug.Log(touchDown2);
+    }
+
+    void CheckTouch3() {
+        touchDown1 = false;
+        Debug.Log(touchDown3);
+    }
+
+	void Start () {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        moveSpeed = 0.1f;
+        stopSpeed = (moveSpeed) / 10f;
+        touchDown1 = false;
+        Debug.Log(touchDown1);
+        touchDown2 = false;
+        Debug.Log(touchDown2);
+        touchDown3 = false;
+        Debug.Log(touchDown3);
+
+
+
+    }
+
+    void FixedUpdate () {
+
+        Vector2 currentPos  = new Vector2();
+
+        Vector2 treePos1 = new Vector2(2.22f, -2.2f);
+
+        Vector2 treePos2 = new Vector2(-4.93f, 1.13f);
+
+        Vector2 treePos3 = new Vector2(3.65f, 2.75f);
+
+        currentPos = transform.position;
+        
+
+        if (touchDown1 == false){
+
+            Vector2 dir1 = (treePos1 - currentPos);
+            rb2d.velocity += (dir1).normalized * moveSpeed;
+            rb2d.velocity = dir1;
+
+            Debug.Log(currentPos);
+            Debug.Log(treePos1);
+            if (Vector2.Distance(currentPos, treePos1) <=0.1f){
+                Invoke("CheckTouch1", 2);
+            }
+
+            
+        }
+
+        
+
+        if (touchDown1 == true){
+            Vector2 dir2 = (treePos2 - currentPos);
+            rb2d.velocity += (dir2).normalized * moveSpeed;
+            rb2d.velocity = dir2;
+            Debug.Log(currentPos);
+            if (Vector2.Distance(currentPos, treePos2) < 0.1f){
+                Invoke("CheckTouch2", 3);
+            }
+
+
+
+        }
+
+                     
+        if (touchDown2 == true){
+            Vector2 dir3 = (treePos3 - currentPos);
+            rb2d.velocity += (dir3).normalized * moveSpeed;
+            rb2d.velocity = dir3;
+            Debug.Log(currentPos);
+
+            if (Vector2.Distance(currentPos, treePos3) < 0.1f){
+                Invoke("CheckTouch3", 3);
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
