32a281c0005d407e735d9bd6e556a026a2efc425
Mark Raper <mrkraper@gmail.com>
2016-09-14 21:59:45 +0100

    stay on target

--- BirdMove.cs
+++ BirdMove.cs
@@ -67,16 +67,16 @@ public class BirdMove : MonoBehaviour
         if (touchDown1 == false)
         {
             Debug.Log(touchDown1);
-            Vector2 dir1 = (treePos1 - currentPos);
+            Vector2 dir1 = (treePos2 - currentPos);
             rb2d.velocity += (dir1) * moveSpeed / Time.deltaTime;
             rb2d.velocity = (dir1);
 
             Debug.Log(currentPos);
-            Debug.Log(treePos1);
-            if (Vector2.Distance(currentPos, treePos1) <= 0.1f)
+            Debug.Log(treePos2);
+            if (Vector2.Distance(currentPos, treePos2) <= 0.1f)
             {
                 Invoke("CheckTouch1", 2);
-                float angle = Mathf.Atan2(treePos3.y, treePos3.x) * Mathf.Rad2Deg;
+                float angle = Mathf.Atan2(treePos1.y, treePos1.x) * Mathf.Rad2Deg;
                 transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
                 Debug.Log(angle);
                 //rb2d.MoveRotation(60);
@@ -90,16 +90,16 @@ public class BirdMove : MonoBehaviour
 
         if (touchDown1 == true)
         {
-            Vector2 dir2 = (treePos2 - currentPos);
+            Vector2 dir2 = (treePos3 - currentPos);
             rb2d.velocity += (dir2).normalized * moveSpeed;
             rb2d.velocity = dir2;
             Debug.Log(currentPos);
 
-            if (Vector2.Distance(currentPos, treePos2) < 0.1f)
+            if (Vector2.Distance(currentPos, treePos3) < 0.1f)
             {
                 //rb2d.MoveRotation(-70);
                 Invoke("CheckTouch2", 2);
-                float angle = Mathf.Atan2(treePos1.y, treePos1.x) * Mathf.Rad2Deg;
+                float angle = Mathf.Atan2(treePos2.y, treePos2.x) * Mathf.Rad2Deg;
                 transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
             }
@@ -110,17 +110,17 @@ public class BirdMove : MonoBehaviour
 
         if (touchDown2 == true)
         {
-            Vector2 dir3 = (treePos3 - currentPos);
+            Vector2 dir3 = (treePos1 - currentPos);
             rb2d.velocity += (dir3).normalized * moveSpeed;
             rb2d.velocity = dir3;
             Debug.Log(currentPos);
 
-            if (Vector2.Distance(currentPos, treePos3) < 0.1f)
+            if (Vector2.Distance(currentPos, treePos1) < 0.1f)
             {
 
                 //rb2d.MoveRotation(170);
-                Invoke("CheckTouch3", 0);
-                float angle = Mathf.Atan2(treePos2.y, treePos2.x) * Mathf.Rad2Deg;
+                Invoke("CheckTouch3", 2);
+                float angle = Mathf.Atan2(treePos3.y, treePos3.x) * Mathf.Rad2Deg;
                 transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
             }
