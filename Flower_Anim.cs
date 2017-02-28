42700ddc21a324e81bfed4f5b85e43146fdc7629
Mark Raper <mrkraper@gmail.com>
2017-02-28 21:35:58 +0000

    Adding flower anim script

---
+++ Flower_Anim.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flower_Anim : MonoBehaviour
+{
+    private Animator animator;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }    
+	
+    void OnTriggerEnter2D(Collider2D Other)
+    {
+        if(Other.gameObject.CompareTag("Player"))
+        {
+            animator.Play("FlowerInter");
+        }
+    }	
+	
+    void OnTriggerExit2D(Collider2D Other)
+    {
+        if(Other.gameObject.CompareTag("Player"))
+        {
+            animator.Play("FlowerIdle");
+        }
+    }
+}
